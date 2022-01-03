1d1a3de248f887e3b0667378a51a513132461da5
Noam <noamsoudai@gmail.com>
2022-01-03 19:15:01 +0200

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import "./Product.css";
+
+export default function Product({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+  rating,
+  rate,
+  count,
+}) {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img src={image} alt="img" />
+      </div>
+      <div className="product-info">
+        <h5> {description}</h5>
+        <h6> {price}</h6>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Products/Products.js
@@ -0,0 +1,19 @@
+import Product from "../Product/Product";
+import "./Products.css";
+
+export default function Products(props) {
+  return (
+    <ul className="products">
+      {props.products.map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          image={product.image}
+          description={product.description}
+        />
+      ))}
+    </ul>
+  );
+}
